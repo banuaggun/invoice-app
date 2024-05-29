b66435151c5057497fc5b0b77109c7ba1390bf6c
banuaggun <banuaggun@outlook.com>
2024-05-29 14:14:09 +0300

    Added dark & light mode

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import {useState} from 'react'
+import useDarkMode from '../hooks/useDarkMode'
+
+
+function Header() {
+
+  const [colorTheme, setColorTheme] =useDarkMode()
+  const [darkSide, setDarkSide] = useState(
+    colorTheme === 'light'? true : false
+  )
+
+  const toggleDarkMode = () => {
+    setColorTheme(colorTheme)
+    setDarkSide((state) => !state)
+  }
+
+  return (
+    <header>
+      <div>
+        {/* dark - light mode button */}
+        {
+            colorTheme === 'light' ? <button className='header-button-light' style={{width:'44px', height:'44px'}} onClick={toggleDarkMode}>
+              <i style={{color:'#000', fontSize:'24px'}} class="ph-fill ph-sun-dim"></i>
+            </button> : <button className='header-button-dark' style={{width:'44px', height:'44px'}} onClick={toggleDarkMode}>
+              <i style={{fontSize:'24px'}} class="ph-fill ph-moon-stars"></i>
+            </button>
+          }
+      </div>
+    </header>
+  )
+}
+
+export default Header
