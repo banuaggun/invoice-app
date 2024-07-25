420c497c88ff6e2578d7354ce7a6b93276c4f5e1
banuaggun <banuaggun@outlook.com>
2024-07-25 15:56:17 +0300

    Table view is creating for responsive structure

--- src/components/InvoiceCard.jsx
+++ src/components/InvoiceCard.jsx
@@ -6,6 +6,24 @@ import '../assets/styles/invoice-card.css'
 function InvoiceCard({invoice}) {
   return (
     <div>
+      <table className="mobile-card">
+        <tr>
+       <tr className='mobile-card-row-1'>
+         <td className='col-1'>
+          {invoice.id}
+         </td>
+         <td className='col-2'>{invoice.paymentDue}</td>
+       </tr>
+       <tr>
+         <td className='col-1'>{invoice.clientName}</td>
+         <td className='col-2'>{invoice.total}</td>
+       </tr>
+       <td className='col-3'>{invoice.status}
+       <i className="ph-bold ph-caret-right"></i>
+       </td>
+  
+       </tr>
+      </table>
       <div className="card">
         <div className="card-content">
           <div className="card-content-id">
--- src/components/MainArea.jsx
+++ src/components/MainArea.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import {useState} from 'react'
 import { useSelector } from 'react-redux'
 import '../assets/styles/main-area.css'
+import Info from './info'
 import InvoiceCard from './InvoiceCard'
 import InvoiceInfo from './InvoiceInfo'
 
@@ -10,15 +11,46 @@ function MainArea() {
   const invoices = useSelector((state) => state.invoices.allInvoice)
 
   console.log(invoices)
+
+  const [filteredInvoices, setFilteredInvoices] = useState(invoices);
+
+  /*
+  const handleFilterChange = (selectedValue) => {
+    if (selectedValue === 'all') {
+      setFilteredInvoices(invoices);
+    } else{
+      const filtered = invoices.filter((invoice) => invoice.status === selectedValue);
+      setFilteredInvoices(filtered);
+    }
+  };
+
+
+  const handleFilterChange = (selectedValue) => {
+    if (selectedValue === "all") {
+      setFilteredInvoices(invoices);
+    } else if (selectedValue === "paid") {
+      setFilteredInvoices(allInvoices.filter(invoice => invoice.status === "paid"));
+    } else if (selectedValue === "pending") {
+      setFilteredInvoices(allInvoices.filter(invoice => invoice.status === "pending"));
+    } else if (selectedValue === "draft") {
+      setFilteredInvoices(allInvoices.filter(invoice => invoice.status === "draft"));
+    }
+  };
+  
+  */
   return (
     <div className='main-area main-area-mobile main-area-tablet main-area-web'>
      <InvoiceInfo invoices={invoices} />
 
+     {/* 
+<Info invoices={invoices} handleFilterChange={handleFilterChange}/>
+  */}
+
       <div className="main-area-content">
         {
-          invoices.map((invoice, index) =>{
+          filteredInvoices.map((invoice, index) =>{
             return(
-              <div>
+              <div key={index}>
                 <InvoiceCard invoice={invoice} />
               </div>
             )
