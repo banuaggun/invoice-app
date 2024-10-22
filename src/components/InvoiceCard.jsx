40003f8d636c5bcf51b9038c3ebbd872cf2ee142
banuaggun <banuaggun@outlook.com>
2024-10-22 16:55:36 +0300

    The route structure of the card has been edited.

--- src/components/InvoiceCard.jsx
+++ src/components/InvoiceCard.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import PaidStatus from './PaidStatus'
 import '../assets/styles/invoice-card.css'
+import { Link } from 'react-router-dom'
 
 
 function InvoiceCard({invoice}) {
+
   return (
+    <Link to={`/invoice/${invoice.id}`}>
     <div>
       <div className="mobile-invoice-card">
         <div className="row-1">
           {invoice.id}
+          <div className="right-button">
+        <i className="ph-bold ph-caret-circle-right"></i>
+        </div>
         </div>
         <div className="row-2">
           <span>{invoice.paymentDue}</span>
@@ -20,42 +26,9 @@ function InvoiceCard({invoice}) {
           <span>{invoice.clientName}</span>
           <span>{invoice.total} $</span>
         </div>
-
-        <div className="right-button">
-        <i className="ph-bold ph-caret-circle-right"></i>
-        </div>
       </div>
-    {/*
-          <table className="mobile-card">
-        <tr className="mobile-card-row">
-          <tr>
-            <th rowSpan={2} className="mobile-card-id">
-              {invoice.id}
-            </th>
-            <td>
-              {invoice.paymentDue} 
-            </td>
-            <td className='column-3'>
-              {invoice.status}
-            </td>
-            <th rowSpan={2} className="mobile-card-arrow">
-              <i className="ph-bold ph-caret-right"></i>
-            </th>
-          </tr>
-          <tr>
-            <td>
-              {invoice.clientName}
-            </td>
-            <td className='column-3'>
-              {invoice.total}
-            </td>
-          </tr>
-        </tr>
-       
-     
-      </table>
-  */}
     </div>
+    </Link>
   )
 }
 
